9ec2b9919d0d05296bf12c1a4fed9ded36194804
Keshava Grama <Keshava.Grama@civica.co.uk>
2018-03-02 15:31:20 +0000

    Removed unnessary files and cleaned up pom

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# Build and deploy the Maven code to S3.
+# Build and test
 export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$ACCESS_KEY
 mvn -B compile
 mvn -B test
-mvn -B deploy -DskipTests
